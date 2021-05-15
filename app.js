430658108af6318a771250cc29b50a7b564077bd
Pablo Domínguez <pablo.dominguezb@gmail.com>
2021-05-15 11:17:25 +0200

    Level 4 bcrypt (hash + salt + round)

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const md5 = require('md5');
+// const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const  app = express();
 
@@ -34,14 +36,16 @@ app.route("/login")
 })
 .post((req,res)=>{
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   User.findOne({user: username}, (err,foundUser)=>{
     if(err){console.log(err);} else {
       if(foundUser) {
-        if (foundUser.password === password) {
-          res.render("secrets");
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if (result === true) {
+            res.render("secrets");
+          }
+        });
       }
     }
   })
@@ -53,17 +57,19 @@ app.route("/register")
   res.render("register");
 })
 .post((req,res)=>{
-  const newUser = new User ({
-    user: req.body.username,
-    password: md5(req.body.password)
-  });
-  newUser.save((err)=>{
-    if (!err) {
-      console.log("New user created");
-      res.render("secrets");
-    } else {
-      console.log(err);
-    }
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User ({
+      user: req.body.username,
+      password: hash
+    });
+    newUser.save((err)=>{
+      if (!err) {
+        console.log("New user created");
+        res.render("secrets");
+      } else {
+        console.log(err);
+      }
+    })
   })
 });
 
